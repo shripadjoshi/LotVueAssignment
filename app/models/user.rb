24991d2e629dba9ebbc723ade8f480cb32a347e0
Shripad Joshi (C) <sjoshi@splunk.com>
2019-03-16 17:06:48 +0530

    added avtar and modified the view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :roles
+  has_one_attached :avatar
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
 end
